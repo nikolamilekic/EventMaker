37dc037f0e5cdc43ec9e87b05ecef051e5754709
Nikola Milekic <nikolamilekic@gmail.com>
2018-12-27 10:03:04 +0100

    Initial commit

---
+++ build.fsx
@@ -0,0 +1,99 @@
+#r "paket:
+nuget Fake.Core.Target
+nuget Fake.DotNet.Cli
+nuget Fake.IO.Zip
+nuget Fake.DotNet.AssemblyInfoFile
+nuget FSharp.Data
+nuget FSharp.Core //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open System.IO
+
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open FSharp.Data
+
+let productName = "EventMaker"
+let solution = productName + ".sln"
+let binPath = "src/EventMaker/bin/Release/netcoreapp2.1"
+let publishPath = binPath </> "publish"
+let zipFileName = "publish.zip"
+let buildNumber = Environment.environVarOrNone "APPVEYOR_BUILD_NUMBER"
+
+Target.create "Clean" <| fun _ ->
+    Seq.allPairs [|"src"|] [|"bin"; "obj"|]
+    |> Seq.collect (fun (x, y) -> !!(sprintf "%s/**/%s" x y))
+    |> Seq.append [|"bin"; "obj"|]
+    |> Shell.deleteDirs
+
+    File.delete "publish.zip"
+Target.create "Build" <| fun _ -> DotNet.build id solution
+Target.create "Publish" <| fun _ -> DotNet.publish id solution
+Target.create "CopyBuildOutput" <| fun _ ->
+    Shell.copyDir "bin" binPath (fun _ -> true)
+Target.create "Zip" <| fun _ ->
+    !! (publishPath </> "**")
+    |> Zip.zip publishPath zipFileName
+Target.create "Release" <| fun _ ->
+    let body =
+        File.readAsBytes zipFileName
+        |> HttpRequestBody.BinaryUpload
+    let response =
+        Http.Request(
+            "https://EventMaker.scm.azurewebsites.net/api/zipdeploy",
+            headers =
+                [
+                    "Authorization",
+                        "Basic " + (Environment.environVarOrFail "AzureDeploymentToken")
+                ],
+            httpMethod = HttpMethod.Post,
+            body = body
+        )
+
+    if response.StatusCode <> 200 then response.ToString() |> failwith
+Target.create "UpdateAssemblyInfo" <| fun _ ->
+    let (|Fsproj|Csproj|) (projectFileName : string) =
+        match projectFileName with
+        | f when f.EndsWith("fsproj") -> Fsproj
+        | f when f.EndsWith("csproj") -> Csproj
+        | _ -> failwith (sprintf "Project file %s not supported. Unknown project type." projectFileName)
+
+    let version = buildNumber |> Option.defaultValue "9999" |> sprintf "1.%s.0.0"
+    !! "src/**/*.??proj"
+    |> Seq.iter (fun projectPath ->
+        let projectName = Path.GetFileNameWithoutExtension projectPath
+        let directoryName = Path.GetDirectoryName projectPath
+        let attributes = [
+            AssemblyInfo.Title projectName
+            AssemblyInfo.Product productName
+            AssemblyInfo.Version version
+            AssemblyInfo.FileVersion version
+        ]
+        match projectPath with
+        | Fsproj ->
+            AssemblyInfoFile.createFSharp
+                (directoryName </> "AssemblyInfo.fs")
+                attributes
+        | Csproj ->
+            AssemblyInfoFile.createCSharp
+                (directoryName </> "Properties" </> "AssemblyInfo.cs")
+                attributes)
+
+Target.create "appveyor" ignore
+
+"Clean"
+==> "Build"
+==> "CopyBuildOutput"
+==> "Publish"
+==> "Zip"
+==> "Release"
+==> "appveyor"
+
+"UpdateAssemblyInfo" ?=> "Build"
+"UpdateAssemblyInfo" ==> "appveyor"
+
+Target.runOrDefault "CopyBuildOutput"
---
+++ src/EventMaker/Functions.fs
@@ -0,0 +1,257 @@
+
+module EventMaker.Functions
+
+open System
+open System.IO
+open System.Globalization
+open System.Reflection
+open Microsoft.Azure.WebJobs
+open Microsoft.Extensions.Configuration
+open Microsoft.WindowsAzure.Storage.Blob
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+open Microsoft.Azure.WebJobs.Hosting
+open Microsoft.Azure.WebJobs.Host.Config
+open Microsoft.Azure.WebJobs.Description
+
+open FSharp.Data
+open Milekic.YoLo
+
+type Config = {
+    FacebookPath : string
+    GoogleAccessTokenRequest : HttpRequestBody
+}
+
+[<AttributeUsage(AttributeTargets.Parameter, AllowMultiple = true)>]
+[<Binding>]
+type ConfigAttribute() = inherit Attribute()
+
+type ConfigProvider(configuration : IConfiguration) =
+    let config = {
+        FacebookPath = configuration.["FacebookPath"]
+        GoogleAccessTokenRequest =
+            [
+                "client_secret", configuration.["GoogleClientSecret"]
+                "grant_type", "refresh_token"
+                "refresh_token", configuration.["GoogleRefreshToken"]
+                "client_id", configuration.["GoogleClientId"]
+            ] :> seq<_>
+            |> HttpRequestBody.FormValues
+    }
+    interface IExtensionConfigProvider with
+        member __.Initialize context =
+            context
+                .AddBindingRule<ConfigAttribute>()
+                .BindToInput(fun _ -> config)
+            |> ignore
+
+type Startup() =
+    interface IWebJobsStartup with
+        member __.Configure builder =
+            builder.AddExtension<ConfigProvider>() |> ignore
+
+[<assembly: WebJobsStartup(typedefof<Startup>)>]
+do ()
+
+[<Literal>]
+let ContactsPath = """PeopleList.json"""
+let userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+
+type GoogleAccessToken = JsonProvider<"""{
+  "access_token": "ya29.Gls4BnIjINLP7XSfOOSxFc2JPjnxQPTKyMB01lfViP9dnGXGjLUbrjbBzx0lSzRYdkBPKqQXtIuul2PxW5L4aS6GUjQA1PvuNp88ANt-Aw3C5VGW_m1Ou3i6gvOK",
+  "scope": "https://www.googleapis.com/auth/contacts.readonly",
+  "expires_in": 3600,
+  "token_type": "Bearer"
+}""">
+
+let getGoogleHeaders config = async {
+    let! accessTokenResponse =
+        Http.AsyncRequestString (
+            "https://www.googleapis.com/oauth2/v4/token",
+            body = config.GoogleAccessTokenRequest,
+            httpMethod = "POST"
+        )
+    return
+        accessTokenResponse
+        |> GoogleAccessToken.Parse
+        |> fun t -> t.AccessToken
+        |> sprintf "Bearer %A"
+        |> fun x -> Seq.singleton ("Authorization", x)
+}
+
+type Contacts = JsonProvider<ContactsPath>
+
+let getEventsFromContacts googleHeaders = async {
+    let getYear = function | 1604 -> None | y -> Some y
+
+    let! rawContacts =
+        Http.AsyncRequestString(
+            "https://people.googleapis.com/v1/people/me/connections?personFields=names,birthdays,events&pageSize=2000",
+            headers = googleHeaders
+        )
+    return
+        rawContacts
+        |> Contacts.Parse
+        |> fun r -> r.Connections
+        |> Seq.where (fun c -> c.Names |> Seq.isEmpty |> not)
+        |> Seq.collect (fun c -> seq {
+            let name = c.Names |> Seq.head |> fun n -> n.DisplayName
+
+            yield!
+                c.Birthdays
+                |> Seq.map (fun b ->
+                    let eventName = sprintf "%s's Birthday" name
+                    let year = b.Date.Year |> Option.bind getYear
+                    eventName, b.Date.Month, b.Date.Day, year)
+            yield!
+                c.Events
+                |> Seq.map (fun e ->
+                    let eventName = sprintf "%s's %s" name e.FormattedType
+                    let year = e.Date.Year |> Option.bind getYear
+                    eventName, e.Date.Month, e.Date.Day, year)
+        })
+}
+
+let getEventsFromFacebook facebookPath = async {
+    let folder (items, name, date) (input : string) =
+        let split = input.Split(':')
+        if split.Length = 0 then (items, name, date) else
+        let command = split |> Seq.head
+        let args = split |> Array.skip 1 |> fun args -> String.Join(":", args)
+        match command, args with
+        | "BEGIN", "VEVENT" -> items, None, None
+        | "DTSTART", start -> items, name, Some start
+        | "SUMMARY", name -> items, Some name, date
+        | "END", "VEVENT" ->
+            match name, date with
+            | Some n, Some d ->
+                let n = n.Replace("birthday", "Birthday")
+                let d = DateTime.ParseExact(d, "yyyyMMdd", CultureInfo.InvariantCulture)
+                (n, d.Month, d.Day, Option<int>.None) :: items, None, None
+            | _ -> items, None, None
+        | _ -> (items, name, date)
+
+    let! rawIcs =
+        Http
+            .AsyncRequestString(
+                facebookPath,
+                headers = [ "User-Agent", userAgent ])
+    let ics = rawIcs.Split([|"\r\n"|], StringSplitOptions.RemoveEmptyEntries)
+
+    let items, _, _ =
+        ics
+        |> Seq.fold folder ([], None, None)
+
+    return items |> Seq.ofList
+}
+
+let getEventsFromPrevious (raw : string) =
+    raw.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map (fun l ->
+        let nameDateSplit = l.Split(';')
+        let n = nameDateSplit.[0]
+        let dateSplit = nameDateSplit.[1].Split('/')
+        let m = int dateSplit.[0]
+        let d = int dateSplit.[1]
+        if dateSplit.Length = 3
+        then n, m, d, Some dateSplit.[2] |> Option.map int
+        else n, m, d, None)
+
+let getFutureDate (currentTime : DateTime) date =
+    if date > currentTime
+    then date
+    else DateTime(currentTime.Year + 1, date.Month, date.Day)
+
+let run config previous startDate endDate = async {
+    let currentTime = DateTime.Now
+    let getFutureDate = getFutureDate currentTime
+    let! facebookRetriever =
+        getEventsFromFacebook config.FacebookPath |> Async.StartChild
+    let! googleRetriever =
+        async {
+            let! googleHeaders = getGoogleHeaders config
+            return! getEventsFromContacts googleHeaders
+        }
+        |> Async.StartChild
+
+    let! fromFacebook = facebookRetriever
+    let! fromContacts = googleRetriever
+    let previous = getEventsFromPrevious previous
+
+    let complete =
+        [ fromContacts; fromFacebook; previous ]
+        |> Seq.concat
+        |> Seq.groupBy (fun (n, m, d, _) -> n, m, d)
+        |> Seq.map (fun (_, g) -> g |> Seq.sort |> Seq.last)
+        |> Seq.sortBy (fun (_, m, d, _) -> m, d)
+        |> List.ofSeq
+
+
+    let state =
+        complete
+        |> Seq.map (fun (n, m, d, y) ->
+            match y with
+            | Some y -> sprintf "%s;%d/%d/%d" n m d y
+            | None -> sprintf "%s;%d/%d" n m d)
+        |> fun s -> String.Join(Environment.NewLine, s)
+
+    let tasks =
+        complete
+        |> Seq.map (fun (n, m, d, y) ->
+            let date = DateTime(currentTime.Year, m, d) |> getFutureDate
+            let name =
+                match y with
+                | None -> n
+                | Some birthYear ->
+                    let age = date.Year - birthYear
+                    sprintf "%s (%i)" n age
+            name, date)
+        |> Seq.where(fun (_, date) -> date >= startDate && date < endDate)
+        |> Seq.map (fun (name, date) ->
+            let formattedDate = date.ToString("yyyy-MM-dd")
+            sprintf
+                "- %s @due(%s 20:00) @defer(%s) @tags(Anywhere)"
+                name
+                formattedDate
+                formattedDate)
+
+    return tasks, state
+}
+
+[<FunctionName("GetTasks")>]
+let getTasks
+    (
+        [<HttpTrigger(AuthorizationLevel.Function, "get", Route = "events/{month}")>]
+            (request : HttpRequest),
+        month : string,
+        [<Config>] config,
+        [<Blob("events/Events.txt", FileAccess.Read)>] previous,
+        [<Blob("events/Events.txt", FileAccess.Write)>] (nextState : CloudBlockBlob)
+    ) =
+    async {
+        let startDate, endDate =
+            let currentTime = DateTime.Now
+            let currentYearStart =
+                let month = Int32.Parse month
+                if month < 1 then failwith "Month cannot be < 1"
+                if month > 12 then failwith "Month cannot be > 12"
+                DateTime(currentTime.Year, month, 1)
+            let currentYearEnd = currentYearStart.AddMonths 1
+            if currentYearEnd < currentTime then
+                currentYearStart.AddYears 1,
+                currentYearEnd.AddYears 1
+            else currentYearStart, currentYearEnd
+
+        let! tasks, state = run config previous startDate endDate
+        do! nextState.UploadTextAsync(state) |> Async.AwaitTask
+        return String.Join(Environment.NewLine, tasks)
+    }
+    |> Async.StartAsTask
+
+[<FunctionName("GetVersion")>]
+let getVersion
+    (
+        [<HttpTrigger(AuthorizationLevel.Function, "get")>]
+            (request : HttpRequest)
+    ) =
+    Assembly.GetExecutingAssembly().GetName().Version.ToString()
